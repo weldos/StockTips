a6334f4376f628a6ecbe54be3d547710a5f4b7af
weldos <61213730+weldos@users.noreply.github.com>
2020-11-10 14:28:36 +1100

    Update AppNavBar.js

--- client/src/components/layout/AppNavBar.js
+++ client/src/components/layout/AppNavBar.js
@@ -1,7 +1,7 @@
 // React and Bootstrap imports
 import React, { Fragment } from "react";
 import { useHistory } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 //Redux and Firebase imports
 import { compose } from "redux";
@@ -9,7 +9,7 @@ import { connect } from "react-redux";
 import { withFirestore } from "react-redux-firebase";
 
 //FontAwesome and Styling imports
-import { faCommentDollar, faSignal, faHome, faNewspaper, faSms, faEnvelopeOpenText, faUserPlus, faUsers, faSignInAlt, faSignOutAlt  } from '@fortawesome/free-solid-svg-icons'; 
+import { faCommentDollar, faSignal, faHome, faNewspaper, faSms, faEnvelopeOpenText, faUserPlus, faUsers, faSignInAlt, faSignOutAlt, faUser  } from '@fortawesome/free-solid-svg-icons'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 
@@ -57,7 +57,7 @@ const AppNavBar = ({ firebase, auth }) => {
       .catch((error) => console.log(error));
   };
 
-
+  const title = <span><FontAwesomeIcon icon={faUser}/> Admin Panel</span> 
 
   // if user is logged in
   const loggedIn = (
@@ -65,10 +65,17 @@ const AppNavBar = ({ firebase, auth }) => {
         <Nav.Link href='/signup'><FontAwesomeIcon icon={faNewspaper}/>{' '}Newsletter</Nav.Link>
         <Nav.Link href='/text'><FontAwesomeIcon icon={faSms}/>{' '}Text Message</Nav.Link>
         <Nav.Link href='/contact'><FontAwesomeIcon icon={faEnvelopeOpenText}/>{' '}Contact us</Nav.Link>
-        <Nav.Link href='/users'><FontAwesomeIcon icon={faUsers}/>{' '}Users</Nav.Link>
-        <Nav.Link href='/users/register'><FontAwesomeIcon icon={faUserPlus}/>{' '}Register</Nav.Link>
+        {/* <Nav.Link href='/users'><FontAwesomeIcon icon={faUsers}/>{' '}Users</Nav.Link>
+        <Nav.Link href='/users/register'><FontAwesomeIcon icon={faUserPlus}/>{' '}Register</Nav.Link> */}
+        <NavDropdown title={title} id="basic-nav-dropdown">
+          <NavDropdown.Item href='/users'><FontAwesomeIcon icon={faUsers}/>{' '}All Users</NavDropdown.Item>
+          <NavDropdown.Item href='/users/register'><FontAwesomeIcon icon={faUserPlus}/>{' '}Register</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey="disabled" disabled>{auth.email}</NavDropdown.Item>
+          <NavDropdown.Item href="#!" onClick={(e) => logOut(encodeURIComponent)}><FontAwesomeIcon icon={faSignOutAlt}/>{' '}Logout</NavDropdown.Item>
+        </NavDropdown>
         <Nav.Link href='/status'><FontAwesomeIcon icon= {faSignal}/>{' '}Server Status</Nav.Link>
-        <Nav.Link href="/#!" onClick={(e) => logOut(encodeURIComponent)}><FontAwesomeIcon icon={faSignOutAlt}/>{' '}Logout</Nav.Link>
+        {/* <Nav.Link href="/#!" onClick={(e) => logOut(encodeURIComponent)}><FontAwesomeIcon icon={faSignOutAlt}/>{' '}Logout</Nav.Link> */}
         <Nav.Link href='/'><FontAwesomeIcon icon={faHome} size = '2x' /></Nav.Link>
   </Fragment>
   );
